2522a51ab430e67228b6ce37076a58c6454e66c9
Sérgio Ramos <mail@sergioramos.me>
2012-10-25 04:26:51 +0100

    added client side support with component/component

--- sgen.js
+++ sgen.js
@@ -1,7 +1,10 @@
 module.exports = function (length) {
-  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split(''),
-      hash = new Array(length)
-
-  for(var i = 0; i < hash.length; i += 1) { hash[i] = chars[Math.floor(Math.random()*62)] }
-  return hash.join('')
-}
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
+  var hash = [];
+  
+  for(var i = 0; i < length; i += 1) {
+    hash.push(chars[Math.floor(Math.random()*62)]);
+  }
+  
+  return hash.join('');
+};
